eee6dcb27a91244fadf9a37d82805d5e326a9309
Dihia <siliana.aitsmail@gmail.com>
2021-04-24 06:52:03 +0100

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { SingleProductComponent } from './components/shop/single-product/single-product.component';
+import { EditProductComponent } from './components/shop/edit-product/edit-product.component';
+import { CartComponent } from './components/shop/cart/cart.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { FooterComponent } from './components/partials/footer/footer.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
+import { AddToCartModalComponent } from './components/partials/modal/add-to-cart-modal/add-to-cart-modal.component';
+import { DeleteProductModalComponent } from './components/partials/modal/delete-product-modal/delete-product-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent,
+    ShopComponent,
+    SingleProductComponent,
+    EditProductComponent,
+    CartComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+    AddToCartModalComponent,
+    DeleteProductModalComponent
   ],
   imports: [
     BrowserModule,
